02f8f34639d681250b9614a8d68050b868d493aa
Johan Yuri Martínez García <johanyuri24@gmail.com>
2025-10-27 08:49:43 -0600

    13-015 Pantalla de Héroe COMPLETO

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-heroe',
-  template: `
-    <p>
-      heroe works!
-    </p>
-  `,
+  templateUrl: './heroe.component.html',
   styles: [
   ]
 })
 export class HeroeComponent implements OnInit {
-  constructor( private activatedRoute: ActivatedRoute) { }
+
+  heroe!: Heroe;
+
+  constructor( private activatedRoute: ActivatedRoute,
+                private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
 
-    this.activatedRoute.params.subscribe( ({ id }) => {
-      console.log( id ); } );
-    
+    this.activatedRoute.params
+    .pipe(
+      switchMap ( ({ id }) => this.heroesService.getHeroePorId( id ) )
+    )
+    .subscribe( heroe => this.heroe = heroe );
+
 
   }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 
@@ -19,7 +21,9 @@ import {MatCardModule} from '@angular/material/card';
     MatButtonModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule,
+    MatProgressSpinnerModule,
   ]
 })
 export class MaterialModule { }

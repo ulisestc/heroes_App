e6538b6876f6e435b6fc8462098e2d7eafb9f357
Johan Yuri Martínez García <johanyuri24@gmail.com>
2025-10-27 09:21:42 -0600

    13-016 Diseño de la pantalla de Héroe COMPLEMENTO

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { HeroesService } from '../../services/heroes.service';
 import { Heroe } from '../../interfaces/heroes.interface';
@@ -7,15 +7,20 @@ import { Heroe } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+  `]
 })
 export class HeroeComponent implements OnInit {
 
   heroe!: Heroe;
 
   constructor( private activatedRoute: ActivatedRoute,
-                private heroesService: HeroesService ) { }
+              private heroesService: HeroesService,
+              private router: Router ) { }
 
   ngOnInit(): void {
 
@@ -24,7 +29,9 @@ export class HeroeComponent implements OnInit {
       switchMap ( ({ id }) => this.heroesService.getHeroePorId( id ) )
     )
     .subscribe( heroe => this.heroe = heroe );
+  }
 
-
+  regresar() {
+    this.router.navigate(['/heroes/listado']);
   }
 }
--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -5,11 +5,6 @@ import { Heroe } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [`
-    mat-card {
-      margin-top: 20px;
-    }
-  `]
 })
 export class ListadoComponent implements OnInit {
 
